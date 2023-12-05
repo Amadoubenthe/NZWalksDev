30488b8c7814c20a2380de3cbf1235ede0bf0c66
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-12-05 10:45:16 +0000

    Add Models and DbContext

---
+++ NZWalksDev.DataAccess/Models/Region.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalksDev.DataAccess.Models
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+
+    }
+}
