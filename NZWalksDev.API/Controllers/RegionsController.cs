89db6005464b244eb6ff5e118a748d853bf8c2ad
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-12-05 15:18:34 +0000

    Get GetRegions

---
+++ NZWalksDev.API/Controllers/RegionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalksDev.DataAccess.Data;
+
+namespace NZWalksDev.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var regions = _dbContext.Regions.ToList();
+
+            return Ok(regions);
+        }
+    }
+}
